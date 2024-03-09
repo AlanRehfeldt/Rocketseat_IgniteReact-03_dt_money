f7fbd5e77b01af9a16c18025c6a7e43b10e12247
Alan Rehfeldt <alanrehfeldt@gmail.com>
2024-03-08 21:39:47 -0300

    buscando transacoes

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -15,6 +15,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextType {
   transactions: Transaciton[];
+  fetchTransactios: (query?: string) => Promise<void>;
 }
 
 export const TransactionsContext = createContext({} as TransactionsContextType);
@@ -22,18 +23,24 @@ export const TransactionsContext = createContext({} as TransactionsContextType);
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaciton[]>([]);
 
-  async function loadTransactios() {
-    const response = await fetch('http://localhost:3000/transactions');
+  async function fetchTransactios(query?: string) {
+    const url = new URL('http://localhost:3000/transactions')
+
+    if(query) {
+      url.searchParams.append('q', query)
+    }
+
+    const response = await fetch(url);
     const data = await response.json();
     setTransactions(data);
   }
 
   useEffect(() => {
-    loadTransactios()
+    fetchTransactios()
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions }} >
+    <TransactionsContext.Provider value={{ transactions, fetchTransactios }} >
       { children }
     </TransactionsContext.Provider>
   )
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string()
@@ -11,6 +13,7 @@ const searchFormSchema = z.object({
 type SearchFormImputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { fetchTransactios } = useContext(TransactionsContext)
   const { 
     register, 
     handleSubmit,
@@ -21,7 +24,7 @@ export function SearchForm() {
 
   async function handleSearchTransaction(data: SearchFormImputs) {
     await new Promise(resolve => setTimeout(resolve, 2000))
-    console.log(data)
+    await fetchTransactios(data.query)
   }
 
   return (
